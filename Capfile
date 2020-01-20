a60dafe4800582ed4ef5e01941dc8c08ffad21e4
William Henry <william@twomatesmedia.com.au>
2020-01-20 10:48:35 +1000

    more files

--- Capfile
+++ Capfile
@@ -8,6 +8,7 @@ require 'capistrano/rails'
 require 'capistrano/bundler'
 require 'capistrano/rvm'
 require 'capistrano/puma'
+install_plugin Capistrano::Puma
 
 # Load the SCM plugin appropriate to your project:
 #
--- config/deploy.rb
+++ config/deploy.rb
@@ -4,12 +4,19 @@ lock "~> 3.11.2"
 server '13.210.246.6', port: 22, roles: [:web, :app, :db], primary: true
 
 set :application, "rails_learning"
+
+# need to change the repo url
+# set up the ssh key on staging
+# change the ssh url
+# change ssh user
+# change ssh key
+
 set :repo_url, "git@github.com:williamhenry94/rails_learning.git"
 
 # user ssh
-set :user 'ubuntu'
-set :puma_threads [4,16]
-set :puma_workers 0
+set :user, 'ubuntu'
+set :puma_threads, [4,16]
+set :puma_workers, 0
 
 
 # Don't change these unless you know what you're doing
@@ -23,7 +30,7 @@ set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/Downloads/APP-DEV-08.pem) }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
